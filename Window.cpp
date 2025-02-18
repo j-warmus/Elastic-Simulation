0db6a7fae5a10500162119a21fce15dd8ac72e5e
josephwarmus <47515543+josephwarmus@users.noreply.github.com>
2025-02-18 23:35:42 +0900

    Removed apple-specific code since I can't test it

--- Window.cpp
+++ Window.cpp
@@ -39,18 +39,6 @@ bool Window::createWindow(const int width, const int height)
 	// 4x antialiasing.
 	glfwWindowHint(GLFW_SAMPLES, 4);
 
-#ifdef __APPLE__ 
-	// Apple implements its own version of OpenGL and requires special treatments
-	// to make it uses modern OpenGL.
-
-	// Ensure that minimum OpenGL version is 3.3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Enable forward compatibility and allow a modern OpenGL context
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
 	// Create the GLFW window, set Window class as user for callbacks
 	m_glfwWindow = glfwCreateWindow(width, height, m_windowTitle.c_str(), NULL, NULL);
 	glfwSetWindowUserPointer(m_glfwWindow, static_cast<void*>(this));
@@ -66,16 +54,12 @@ bool Window::createWindow(const int width, const int height)
 	// Make the context of the window.
 	glfwMakeContextCurrent(m_glfwWindow);
 
-#ifndef __APPLE__
-	// On Windows and Linux, we need GLEW to provide modern OpenGL functionality.
-
 	// Initialize GLEW.
 	if (glewInit())
 	{
 		std::cerr << "Failed to initialize GLEW" << std::endl;
 		return 0;
 	}
-#endif
 
 	// Set swap interval to 1.
 	glfwSwapInterval(0);
@@ -88,10 +72,7 @@ bool Window::createWindow(const int width, const int height)
 
 void Window::resizeCallback(GLFWwindow* window, int width, int height)
 {
-#ifdef __APPLE__
-	// In case your Mac has a retina display.
-	glfwGetFramebufferSize(window, &width, &height); 
-#endif
+
 	Window* windowObj = static_cast<Window*>(glfwGetWindowUserPointer(window));
 	if (!windowObj) exit(EXIT_FAILURE);
 	windowObj->setDimensions(width, height);
--- shader.h
+++ shader.h
@@ -1,12 +1,5 @@
-#ifndef SHADER_HPP
-#define SHADER_HPP
-
-#ifdef __APPLE__
-#include <OpenGL/gl3.h>
-#else
+#pragma once
 #include <GL/glew.h>
-#endif
+
 
 GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path);
-
-#endif
